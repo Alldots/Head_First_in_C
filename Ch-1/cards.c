6842a364d09abd92e8d02e19935deb7a06f013c6
Alldots <alldots@hotmail.com>
2013-03-31 22:54:38 +0800

    cards.c: Loop and Invalid Check Added

--- Ch-1/cards.c
+++ Ch-1/cards.c
@@ -4,27 +4,37 @@
 int main()
 {
     char card_name[3];
-    puts("Enter the card_name: ");
-    scanf("%2s", card_name);
-    int val = 0;
-    switch(card_name[0]) {
-    case 'K':
-    case 'Q':
-    case 'J':
-        val = 10;
-        break;
-    case 'A':
-        val = 11;
-        break;
-    default:
-        val = atoi(card_name);
-    }
+    int count = 0;
+    do {
+        puts("Enter the card_name: ");
+        scanf("%2s", card_name);
+        int val = 0;
+        switch(card_name[0]) {
+        case 'K':
+        case 'Q':
+        case 'J':
+            val = 10;
+            break;
+        case 'A':
+            val = 11;
+            break;
+        case 'X':
+            continue;
+        default:
+            val = atoi(card_name);
+            if (val < 1 || val > 10) {
+                puts("I don't understand that value");
+                continue;
+            }
+        }
     
-    /* Check if the value is 3 to 6 */
-    if (3 <= val && val <= 6)
-         puts("Counts has gone up");
-    /* Otherwise if the card was 10, J, Q or K */
-    else if (val == 10)
-         puts("Counts has gone down");
+        /* Check if the value is 3 to 6 */
+        if ((val > 2) && (val < 7))
+             count++;
+        /* Otherwise if the card was 10, J, Q or K */
+        else if (val == 10)
+             count--;
+        printf("Current count: %i\n", count);
+    } while (card_name[0] != 'X'); 
     return 0;
 }
